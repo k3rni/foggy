30758ffc6f2cf93ffde746b0f7a2e3000b44927a
Krzysztof Zych <k33rni@gmail.com>
2015-08-09 11:29:38 +0200

    Extract more from the EDID block. Write specs for that.

--- edid.lua
+++ edid.lua
@@ -8,21 +8,39 @@ function a2b(str)
 end
 
 function edid.monitor_name(edid_str)
-  local edid = a2b(edid_str)
-  -- We just want the descriptor blocks
-  -- Source: https://en.wikipedia.org/wiki/Extended_Display_Identification_Data
-  -- NOTE: these are zero-based, so we add 1 later
-  local offsets = { { 54, 71 }, { 72, 89 }, { 90, 107 }, { 108, 125 } }
+  return edid.parse_edid(edid_str).monitor_name
+end
 
-  for _, offset in ipairs(offsets) do
+function edid.parse_edid(edid_str)
+  local bytes = a2b(edid_str)
+  local data = {}
+  
+  -- Source: https://en.wikipedia.org/wiki/Extended_Display_Identification_Data
+  -- NOTE: all offsets in the spec are zero-based, while Lua's are one-based.
+
+  local mfr0, mfr1 = string.byte(bytes, 11, 12)
+  data.manufacturer_code = mfr0 + mfr1 * 2^8
+
+  local sn0, sn1, sn2, sn3 = string.byte(bytes, 13, 16)
+  data.serial_number = (sn0 + sn1 * 2^8 + sn2 * 2^16 + sn3 * 2^24)
+
+  data.week_of_manufacture = string.byte(bytes, 17)
+  data.year_of_manufacture = string.byte(bytes, 18) + 1990
+
+  -- Descriptor blocks store things such as monitor name. Zero-based, corrected later.
+  local descriptor_block_offsets = { { 54, 71 }, { 72, 89 }, { 90, 107 }, { 108, 125 } }
+
+  for _, offset in ipairs(descriptor_block_offsets) do
     local low = offset[1] + 1
     local high = offset[2] + 1
-    local desc_type = string.byte(edid:sub(low + 3))
-    if desc_type == 0xFC then -- monitor name
-      local monitor_name = edid:sub(low + 5, high):gsub("[\r\n ]+$", "")
-      return monitor_name
+    local desc_type = string.byte(bytes:sub(low + 3))
+    if desc_type == 0xFC then -- monitor name, space-padded with a LF
+      local monitor_name = bytes:sub(low + 5, high):gsub("[\r\n ]+$", "")
+      data.monitor_name = monitor_name
     end
   end
+
+  return data
 end
 
 return edid
